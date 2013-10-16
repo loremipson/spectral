dee5ac6019d4367110f6d723b88e5c98c1b9af65
Tyler Ipson <tipson@1on1.com>
2013-10-16 13:54:22 -0600

    Adding node dependencies and base Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+(function(){
+  "use strict";
+  
+  module.exports = function(grunt) {
+    
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+    
+    });
+
+  };
+
+})();
