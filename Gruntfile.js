85928ec951eec88eb86c6cd28fed2a748fa4eca7
Tyler Ipson <tipson@1on1.com>
2013-10-16 15:26:38 -0600

    Grunt compiling sass and livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,40 @@
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
-    
+      // Set up paths
+      meta: {
+        sassPath: 'assets/sass',
+        sassExt: '.sass', // .sass or .scss
+        deployPath: 'assets/css'
+      },
+
+      sass: {
+        dist: {
+          options: {
+            style: 'compressed'
+          },
+          files: [{
+            expand: true,
+            cwd: '<%= meta.sassPath %>',
+            src: ['*<%= meta.sassExt %>', '!_*<%= meta.sassExt %>'],
+            dest: '<%= meta.deployPath %>/',
+            ext: '.css'
+          }]
+        }
+      },
+
+      watch: {
+        options: {
+          livereload: true
+        },
+        sass: {
+          files: ['<%= meta.sassPath %>/**/*<%= meta.sassExt %>'],
+          tasks: ['sass']
+        }
+      }
     });
+
+    grunt.registerTask('default', ['watch']);
 
   };
 
